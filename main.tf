6604e3b28b8170e314dd4bcf8f51f0d725c43f2a
briskt <3172830+briskt@users.noreply.github.com>
2023-08-03 17:04:41 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_record" "spacelift" {
   zone_id = data.cloudflare_zone.this.id
-  name    = "spacelift"
+  name    = "spacelift1"
   value   = var.cname_value
   type    = "CNAME"
   proxied = true
